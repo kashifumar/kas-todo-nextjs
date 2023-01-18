72c9c5b3e672f7d5265c8f27263931f28ac0592d
Kashif Umar <Kashif.TLB@gmail.com>
2023-01-18 15:51:44 +0500

    removed todolist component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AddNewTodo from "./add-new-todo";
 import TodoList from "./todo-list";
+import Todo from "./todo";
 
 interface Todo {
   id: number;
@@ -7,11 +8,32 @@ interface Todo {
   completed: boolean;
 }
 
-export default function Page(){
+export default async function Page(){
+
+  let res  = await fetch("https://kas-json-api.vercel.app/api/todo/list");
+  const {todos} = await res.json();
+  
+  // console.log(todos);
+  return (
+    <>
+    <div>
+    <AddNewTodo/>    
+      <ul style={{listStyleType:"none",padding:0}}>
+        {todos.map((t:any)=> {
+          return(<li key={t.id} style={{padding:"5px 0"}}>
+            <Todo todo={t}/>
+          </li>
+          );
+        })}
+        
+      </ul>
+    </div>
+    </>
+  );
   return (
   <div>
-    <AddNewTodo/>    
-    <TodoList/>
+    
+    
   </div>
   );
 }
